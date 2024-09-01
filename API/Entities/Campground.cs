b9c735e91ce85c988bdda7fd3f05c908641220e0
devGee <gee@dev.com>
2024-09-01 15:09:43 +0200

    initial commit

---
+++ API/Entities/Campground.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Entities
+{
+    public class Campground
+    {
+        public String Title { get; set; }
+        public String Price { get; set; }
+        public String Description { get; set; }
+        public String Location { get; set; }
+    }
+}
---
+++ API/Entities/Cities.cs
@@ -0,0 +1,12 @@
+﻿namespace API.Entities
+{
+    public class Cities
+    {
+        public String City { get; set; }
+        public String AccentCity { get; set; }
+        public String ProvinceName { get; set; }
+        public String Latitude { get; set; }
+        public String Longitude { get; set; }
+        public int ProvinceID { get; set; }
+    }
+}
